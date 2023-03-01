bc8ba591ccc61fd575219fd0dcd2f76dd02c2e6d
Sobari06 <126376471+Sobari06@users.noreply.github.com>
2023-03-01 12:03:01 +0700

    Update Adkesmah.py

--- Adkesmah.py
+++ Adkesmah.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
 
 st.title("Program Komplain Masyarakat")
 
@@ -37,3 +42,37 @@ if submit_button:
 # Menampilkan daftar keluhan yang telah dikirim
 st.subheader("Daftar Keluhan")
 st.write(df)
+
+smtp_server = "smtp.gmail.com"
+smtp_port = 587
+email_pengirim = "email@gmail.com"  # Masukkan email pengirim
+password = "password"  # Masukkan password email pengirim
+def kirim_notifikasi_email(penerima, subjek, isi_email):
+    try:
+        # Membuat objek pesan email
+        msg = MIMEMultipart()
+        msg['From'] = email_pengirim
+        msg['To'] = penerima
+        msg['Subject'] = subjek
+
+        # Menambahkan isi pesan email
+        body = isi_email
+        msg.attach(MIMEText(body, 'plain'))
+
+        # Mengirim email menggunakan SMTP server
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(email_pengirim, password)
+        text = msg.as_string()
+        server.sendmail(email_pengirim, penerima, text)
+        server.quit()
+
+        print("Email notifikasi berhasil dikirim ke", penerima)
+    except Exception as e:
+        print("Error:", e) 
+        
+penerima = "email_penerima@example.com"
+subjek = "Notifikasi keluhan diterima"
+isi_email = "Dear pengguna, keluhan Anda sudah kami terima dan akan segera ditindaklanjuti."
+kirim_notifikasi_email(penerima, subjek, isi_email)
+
