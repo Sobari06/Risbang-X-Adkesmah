b8a5c2f74c8cf4a61e4538fce45ba28f627fdb7c
Sobari06 <126376471+Sobari06@users.noreply.github.com>
2023-03-08 12:53:11 +0700

    Update Adkesmah.py

--- Adkesmah.py
+++ Adkesmah.py
@@ -1,78 +1,198 @@
 import streamlit as st
 import pandas as pd
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import datetime
+from streamlit_lottie import st_lottie
+import requests 
+# Google Sheets authentication
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('united-option-379311-32c22a337d18.json', scope)
+client = gspread.authorize(creds)
 
+# Open the Google Sheets file
+sheet = client.open('Keluhan Mahasiswa').sheet1
+#data = sheet.get_all_records()
+# Define the sidebar menu options
+menu = ['Halaman Utama', 'Identitas Penerima Keluhan', 'Survei Kepuasan', 'Frequently Asked Questions', 'Akses Keluhan', 'Complaint Analytics']
+choice = st.sidebar.selectbox('Menu', menu)
 
+# Define function for adding complaint to the Google Sheets file
+def add_complaint(nama, nim, wa, tanggal, jenis_keluhan, deskripsi_keluhan):
+    time_sent = pd.Timestamp.now().strftime("%Y-%m-%d %H:%M:%S")
+    data = [nama, nim, wa, tanggal, jenis_keluhan, deskripsi_keluhan, time_sent, '', '', '']
+    sheet.insert_row(data, index=2)
 
-st.title("Program Komplain Masyarakat")
+# Define function for updating the status of a complaint
+def update_status(row, status):
+    col = 9 if status == 'diterima' else 10
+    sheet.update_cell(row, col, pd.Timestamp.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-# Menambahkan form input untuk informasi keluhan
-st.subheader("Formulir Keluhan")
-nama = st.text_input("Nama")
-alamat = st.text_input("Alamat")
-nomor_telepon = st.text_input("Nomor Telepon")
-jenis_keluhan = st.selectbox("Jenis Keluhan", ["Pelayanan Publik", "Lingkungan Hidup", "Kesehatan", "Pendidikan", "Keamanan"])
-deskripsi_keluhan = st.text_area("Deskripsi Keluhan")
+# Define function for getting the complaint data from the Google Sheets file
+def get_complaints():
+    data = sheet.get_all_values()[1:]
+    df = pd.DataFrame(data, columns=['Nama', 'NIM', 'No WA', 'Tanggal', 'Jenis Keluhan', 'Deskripsi Keluhan', 'Waktu Pengiriman', 'Diterima', 'Ditolak', 'Selesai'])
+    df['Waktu Pengiriman'] = pd.to_datetime(df['Waktu Pengiriman'])
+    df['Diterima'] = pd.to_datetime(df['Diterima'], errors='coerce')
+    df['Ditolak'] = pd.to_datetime(df['Ditolak'], errors='coerce')
+    df['Selesai'] = pd.to_datetime(df['Selesai'], errors='coerce')
+    return df
 
-# Menambahkan tombol submit untuk mengirimkan keluhan
-submit_button = st.button("Kirim Keluhan")
+if choice == 'Halaman Utama':
+     #Mendefinisikan fungsi untuk menampilkan animasi Lottie
+    def load_lottie_url(url: str):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
 
-# Membuat dataframe untuk menyimpan data keluhan
-df = pd.DataFrame(columns=["Nama", "Alamat", "Nomor Telepon", "Jenis Keluhan", "Deskripsi Keluhan"])
+    # Mendefinisikan URL animasi Lottie yang akan ditampilkan
+    url = "https://assets8.lottiefiles.com/packages/lf20_km5er2un.json"
 
-# Jika tombol submit ditekan, tambahkan data keluhan ke dataframe
-if submit_button:
-    df = df.append({"Nama": nama, "Alamat": alamat, "Nomor Telepon": nomor_telepon, "Jenis Keluhan": jenis_keluhan, "Deskripsi Keluhan": deskripsi_keluhan}, ignore_index=True)
-    st.success("Keluhan Anda telah berhasil dikirim.")
+    # Menampilkan animasi Lottie di tampilan utama Streamlit
+    st_lottie(load_lottie_url(url))
 
-    # Mengirim notifikasi otomatis ke fakultas/instansi terkait
-    if jenis_keluhan == "Pelayanan Publik":
-        st.write("Notifikasi telah dikirim ke Fakultas/Instansi terkait: Fakultas/Instansi Pelayanan Publik")
-    elif jenis_keluhan == "Lingkungan Hidup":
-        st.write("Notifikasi telah dikirim ke Fakultas/Instansi terkait: Fakultas/Instansi Lingkungan Hidup")
-    elif jenis_keluhan == "Kesehatan":
-        st.write("Notifikasi telah dikirim ke Fakultas/Instansi terkait: Fakultas/Instansi Kesehatan")
-    elif jenis_keluhan == "Pendidikan":
-        st.write("Notifikasi telah dikirim ke Fakultas/Instansi terkait: Fakultas/Instansi Pendidikan")
-    elif jenis_keluhan == "Keamanan":
-        st.write("Notifikasi telah dikirim ke Fakultas/Instansi terkait: Fakultas/Instansi Keamanan")
+    st.title("Dashboard Pengaduan KM PKU IPB")
+    st.title('Input Keluhan Mahasiswa')
 
-# Menampilkan daftar keluhan yang telah dikirim
-st.subheader("Daftar Keluhan")
-st.write(df)
+    # Get input from user
+    nama = st.text_input('Nama')
+    nim = st.text_input('NIM')
+    wa = st.text_input('No WA')
+    tanggal = st.date_input('Tanggal')
+    jenis_keluhan = st.selectbox('Jenis Keluhan', ['Akademik', 'Non-Akademik'])
+    deskripsi_keluhan = st.text_area('Deskripsi Keluhan')
 
-smtp_server = "smtp.gmail.com"
-smtp_port = 587
-email_pengirim = "email@gmail.com"  # Masukkan email pengirim
-password = "password"  # Masukkan password email pengirim
-def kirim_notifikasi_email(penerima, subjek, isi_email):
-    try:
-        # Membuat objek pesan email
-        msg = MIMEMultipart()
-        msg['From'] = email_pengirim
-        msg['To'] = penerima
-        msg['Subject'] = subjek
+    # Add complaint to Google Sheets file
+    if st.button('Kirim Keluhan'):
+        add_complaint(nama, nim, wa, str(tanggal), jenis_keluhan, deskripsi_keluhan)
+        st.success('Keluhan berhasil dikirim.')
 
-        # Menambahkan isi pesan email
-        body = isi_email
-        msg.attach(MIMEText(body, 'plain'))
+if choice == 'Identitas Penerima Keluhan':
+    #Mendefinisikan fungsi untuk menampilkan animasi Lottie
+    def load_lottie_url(url: str):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
 
-        # Mengirim email menggunakan SMTP server
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
-        server.login(email_pengirim, password)
-        text = msg.as_string()
-        server.sendmail(email_pengirim, penerima, text)
-        server.quit()
+    # Mendefinisikan URL animasi Lottie yang akan ditampilkan
+    url = "https://assets3.lottiefiles.com/packages/lf20_KU3FGB47d6.json"
+    # Menampilkan animasi Lottie di tampilan utama Streamlit
+    st_lottie(load_lottie_url(url))
 
-        print("Email notifikasi berhasil dikirim ke", penerima)
-    except Exception as e:
-        print("Error:", e) 
-        
-penerima = "email_penerima@example.com"
-subjek = "Notifikasi keluhan diterima"
-isi_email = "Dear pengguna, keluhan Anda sudah kami terima dan akan segera ditindaklanjuti."
-kirim_notifikasi_email(penerima, subjek, isi_email)
+    st.title('Identitas Penerima Keluhan')
+
+    st.write('1. Nama Penerima Keluhan')
+    st.write('2. NIM Penerima')
+
+# Survei Kepuasan
+if choice == 'Survei Kepuasan':
+        #Mendefinisikan fungsi untuk menampilkan animasi Lottie
+    def load_lottie_url(url: str):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
+
+    # Mendefinisikan URL animasi Lottie yang akan ditampilkan
+    url = "https://assets7.lottiefiles.com/private_files/lf30_VeGYYQ.json"
+    # Menampilkan animasi Lottie di tampilan utama Streamlit
+    st_lottie(load_lottie_url(url))
+    st.title('Survei Kepuasan')
+    st.write('Silahkan mengisi survei kepuasan berikut ini:')
+    name = st.text_input('Nama:')
+    nim = st.text_input('NIM:')
+    date = st.date_input('Tanggal:', datetime.date.today())
+    date_str = date.strftime('%Y-%m-%d') # ubah tipe data date menjadi string
+    st.write('1. Apakah Anda puas dengan layanan yang diberikan?')
+    satisfaction = st.slider('', 1, 10, 5, key='slider1')
+    st.write('2. Apakah respon terhadap keluhan Anda cepat?')
+    response_time = st.slider('', 1, 10, 5, key='slider2')
+    st.write('3. Apakah penyelesaian keluhan Anda memuaskan?')
+    resolution = st.slider('', 1, 10, 5, key='slider3')
+    st.write('4. Apakah petugas yang menangani keluhan Anda ramah?')
+    friendliness = st.slider('', 1, 10, 5, key='slider4')
+    st.write('5. Apakah keluhan Anda ditangani dengan baik?')
+    handling = st.slider('', 1, 10, 5, key='slider5')
+    st.write('6. Apakah solusi yang diberikan efektif?')
+    effectiveness = st.slider('', 1, 10, 5, key='slider6')
+    st.write('7. Apakah ada komunikasi yang baik antara petugas dan Anda?')
+    communication = st.slider('', 1, 10, 5, key='slider7')
+    st.write('8. Apakah Anda merasa dihargai sebagai pelanggan?')
+    appreciation = st.slider('', 1, 10, 5, key='slider8')
+    st.write('9. Apakah Anda akan merekomendasikan layanan ini ke orang lain?')
+    recommendation = st.slider('', 1, 10, 5, key='slider9')
+    st.write('10. Apakah ada hal yang ingin Anda sampaikan terkait layanan ini?')
+    feedback = st.text_input('', '')
+
+    # Save survey data to Google Sheets
+    data = [name, nim, date, satisfaction, response_time, resolution, friendliness, handling, effectiveness, communication, appreciation, recommendation, feedback]
+    sheet.insert_row(data, 2)
+    st.write('Data survei telah disimpan')
+
+if choice == 'Frequently Asked Questions':
+    #Mendefinisikan fungsi untuk menampilkan animasi Lottie
+    def load_lottie_url(url: str):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
+
+    # Mendefinisikan URL animasi Lottie yang akan ditampilkan
+    url = "https://assets3.lottiefiles.com/packages/lf20_D7l6QPTtOL.json"
+    # Menampilkan animasi Lottie di tampilan utama Streamlit
+    st_lottie(load_lottie_url(url))
+
+    st.title('Frequently Asked Questions')
+    st.write('Berikut adalah beberapa pertanyaan dan jawaban yang sering ditanyakan:')
+    st.write('1. Bagaimana cara melihat status keluhan saya?')
+    st.write('Anda dapat melihat status keluhan Anda di menu "Akses Keluhan" setelah melakukan autentifikasi.')
+    st.write('2. Berapa lama waktu yang diperlukan untuk menanggapi keluhan?')
+    st.write('Kami berusaha menanggapi setiap keluhan dalam waktu 24 jam.')
+    st.write('3. Apa yang harus saya lakukan jika keluhan saya tidak direspon?')
+    st.write('Silakan hubungi kami melalui email atau nomor telepon yang tertera di situs kami.')
+    st.write('4. Apakah saya dapat memberikan feedback tentang pelayanan yang diberikan?')
+    st.write('Tentu saja! Anda dapat memberikan feedback melalui menu "Survei Kepuasan" di situs kami.')
+if choice == 'Akses Keluhan':
+    #Mendefinisikan fungsi untuk menampilkan animasi Lottie
+    def load_lottie_url(url: str):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
+
+    # Mendefinisikan URL animasi Lottie yang akan ditampilkan
+    url = "https://assets1.lottiefiles.com/packages/lf20_huqty7bz.json"
+    # Menampilkan animasi Lottie di tampilan utama Streamlit
+    st_lottie(load_lottie_url(url))
+
+    st.title('Autentifikasi')
+    st.write('Silakan masukkan email dan kata sandi Anda untuk mengakses keluhan:')
+    email = st.text_input('Email')
+    password = st.text_input('Kata Sandi', type='password')
+    if st.button('Login'):
+        if email == 'admin' and password == '12345':
+            st.success('Login berhasil')
+            st.write('Berikut adalah daftar keluhan yang belum ditanggapi:')
+            # Display the list of pending complaints from the database
+            query = 'SELECT * FROM complaints WHERE status = "diterima" ORDER BY timestamp DESC'
+            results = db.execute(query)
+            for row in results:
+                st.write(row)
+        else:
+            st.error('Email atau kata sandi Anda salah. Silakan coba lagi.')
+# 
+if choice == 'Complaint Analytics':
+    #Mendefinisikan fungsi untuk menampilkan animasi Lottie
+    def load_lottie_url(url: str):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
+
+    # Mendefinisikan URL animasi Lottie yang akan ditampilkan
+    url = "https://assets9.lottiefiles.com/packages/lf20_5tl1xxnz.json"
+    # Menampilkan animasi Lottie di tampilan utama Streamlit
+    st_lottie(load_lottie_url(url))
 
